8315fe1f0dcc90e0e1f93db24307ea61695e66c6
KokoFibo <kokonacci@gmail.com>
2022-10-26 12:25:14 +0700

    first commit

---
+++ resources/views/livewire/index.blade.php
@@ -0,0 +1,10 @@
+<div>
+    <div class="card">
+        <div class="card-header">
+            <h3>Data Customer</h3>
+        </div>
+        <div class="card-body">
+            <livewire:customer-table />
+        </div>
+    </div>
+</div>
